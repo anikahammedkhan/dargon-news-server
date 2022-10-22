850905bfcbed68b8b56ef3b5842a2e84c2f01363
Md Anik Ahammed Khan <anikkhan1105@gmail.com>
2022-10-22 15:37:47 +0600

    added news data on the server

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories');
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
     res.send('News API running!');
 })
 app.get('/news-categories', (req, res) => {
     res.send(categories);
+})
+app.get('/news', (req, res) => {
+    res.send(news);
 })
 
 app.listen(port, () => {
